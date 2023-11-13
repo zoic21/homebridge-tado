871f675cc33fa16daa52bb109831c0faa8c538c2
Loïc <1536036+zoic21@users.noreply.github.com>
2023-11-13 12:14:51 +0100

    mode log level

--- src/presencePlatformAccessory.ts
+++ src/presencePlatformAccessory.ts
@@ -17,32 +17,25 @@ export class PresencePlatformAccessory {
     private readonly platform: TadoHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
-
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, 'homebridge-tado')
       .setCharacteristic(this.platform.Characteristic.Model, 'switch')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'presence');
-
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
     this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
-    
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
-
     // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.On).onSet(this.handleOnSet.bind(this));
-
     this.homeId = accessory.context.device.id
   }
 
   updateValue(value){
-    this.platform.log.info('Update switch value to :', value);
-    this.platform.log.info('Current value :', this.service.getCharacteristic(this.platform.Characteristic.On).value);
     if(this.service.getCharacteristic(this.platform.Characteristic.On).value != value){
-      this.platform.log.info('Different switch value :', value);
+      this.platform.log.debug('Update switch value to :', value);
       this.service.updateCharacteristic(this.platform.Characteristic.On, value);
     }
   }
@@ -51,10 +44,12 @@ export class PresencePlatformAccessory {
    * Handle requests to set the "On" characteristic
    */
   handleOnSet(value) {
-    this.platform.log.info('Triggered SET On:'+value);
+   
     if(value){
+      this.platform.log.debug('Triggered presence to away');
       this.platform.Tado.setPresence(this.homeId,'away')
     }else{
+      this.platform.log.debug('Triggered presence to home');
       this.platform.Tado.setPresence(this.homeId,'home')
     }
     this.platform.refreshData()
